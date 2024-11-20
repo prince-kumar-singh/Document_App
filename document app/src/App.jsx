106f367bb75d74538a17a6c2f630dd87d58619e3
Prince Kumar Singh <138196541+prince-kumar-singh@users.noreply.github.com>
2024-11-20 21:32:42 +0530

    added foreground element in app.jsx

--- document app/src/App.jsx
+++ document app/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Background from './components/Background';
+import Foreground from './components/Foreground';
 function App() {
   return (
     <div className='relative w-full h-screen bg-zinc-800'>
       <Background />
+      <Foreground />
     </div>
   );
 }
